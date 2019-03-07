d2f9f99e811893f6a4c20e7eb952f4b49f2a4a03
john <john@3kd.be>
2019-03-08 00:21:52 +0100

    add fcAddress key to Processing generator

--- generator/processing.js
+++ generator/processing.js
@@ -7,8 +7,8 @@ const config = require("../lghs-21.json");
 const crates = config.matrix.cols * config.matrix.rows;
 const ledsByCrate = config.crate.rows * config.crate.cols;
 
-let ledOffset = config.fadecandy.ledOffset;
 let output = {
+  "fcAddress": config.opc[0],
   "nbCrates": crates,
   "spacing": config.processing.spacing,
   "pixelsPerCrate": ledsByCrate,
