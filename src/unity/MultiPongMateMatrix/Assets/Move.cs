1567ed5e29e4b44e78c3bb5d16ffe7d9a1387aa5
john <john@3kd.be>
2019-04-06 18:18:22 +0200

    Add touch input in Unity

--- src/unity/MultiPongMateMatrix/Assets/Move.cs
+++ src/unity/MultiPongMateMatrix/Assets/Move.cs
@@ -5,12 +5,36 @@ public class Move : MonoBehaviour
     public float Speed = 1f;
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         transform.Translate(
             Input.GetAxisRaw("Horizontal") * Speed,
             Input.GetAxisRaw("Vertical") * Speed,
             0
         );
+
+//        if (Input.touchCount <= 0) return;
+
+//        aTouch = Input.GetMouseButton(0);
+//        touchPos = Input.mousePosition;
+
+        bool aTouch;
+        Vector2 touchPos;
+
+        if (Application.platform != RuntimePlatform.Android)
+        {
+            aTouch = Input.GetMouseButton(0);
+            touchPos = Input.mousePosition;
+        }
+        else
+        {
+            aTouch = Input.touchCount > 0;
+            touchPos = Input.GetTouch(0).position;
+        }
+
+        if (aTouch)
+        {
+            transform.position = touchPos / 100;
+        }
     }
 }
